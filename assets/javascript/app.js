048f8b84f3b8cd1bcad3916f7be6b262288beb07
Jesse <laethaka@gmail.com>
2018-05-01 16:07:15 -0700

    trying to get game reset working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,21 +10,41 @@ var questionArray = ['What dinosaur name means "fast thief?"', 'Which of the fol
 var answerArray = [['Nanotyrannus', 'Stegosaurus', 'Velociraptor', 'Pteronodon'], ['Ankylosaurus', 'Brachiosaurus', 'Allosaurus', 'Torvosaurus'], ['Snakes', 'Lizards', 'Frogs', 'Birds'], ['Four', 'Three', 'Two', 'One'], ['Terrible lizard', 'Hungry lizard', 'Killer lizard', 'Evil lizard'], ['Chevrons', 'Kothumus', 'Thagomizer', 'Spinalis capitis'], ['Small car', 'Basketball', 'Lime', 'M&M'], ['China', 'Pakistan', 'Mongolia', 'USA']];
 
 
-//INITIAL START BUTTON
-$('#questionBar').text('Welcome... to Triassic Trivia!!').addClass('btn btn-warning btn-lg btn-block startButton').attr('type', 'button')
+function gameReset() { //GAMEPLAY SECTION
+    $('#questionBar').text('Welcome... to Triassic Trivia!!').addClass('btn btn-warning btn-lg btn-block startButton').attr('type', 'button').removeAttr('style');
+    $('#fourthAnswer').removeClass('btn btn-warning')
 
-$('.startButton').on('click', function() { //START BUTTON CLICKED
-    $('#questionBar').removeClass('btn btn-warning btn-lg btn-block startButton').removeAttr('type').off('click');
-    $('.answerBar').addClass('btn btn-warning');
-    startNextQuestion();
-});
+    $('.startButton').on('click', function() { //START BUTTON CLICKED
+        $('#questionBar').removeClass('btn btn-warning btn-lg btn-block startButton').removeAttr('type').off('click');
+        $('.answerBar').addClass('btn btn-warning').removeClass('invisible');
+        startNextQuestion();
+    });
+
+    $('.answerBar').on('click', function() {//ANSWER CLICK LISTENING
+        if ($(this).attr('class') === 'answerBar btn btn-warning correctAnswer') { //CORRECT ANSWER CLICKED
+            answersCorrect++;
+            $('#reactionImage').attr('src', 'assets/images/correctImg.jpg').css('display','inline-block');
+        } else { //WRONG ANSWER CLICKED
+            answersWrong++;
+            $('#reactionImage').attr('src', 'assets/images/incorrectImg.jpg').css('display', 'inline-block');
+        }
+        questionIndex++; //ITERATION
+        $('#questionBar').css('display', 'none');
+        $('.answerBar').css('display', 'none');  
+        $('.correctAnswer').removeClass('correctAnswer'); //CLEAR CORRECT ANSWER MARKER
+        clearInterval(timer);
+        setTimeout (function() {
+            startNextQuestion();
+        }, 2000)
+    });
+};
 
 //TIMER ITERATION
 function timerTick() {
     timeLeft--;
     if (timeLeft==0) { //QUESTION TIMED OUT
         answersTimedOut++;
-        $('#reactionImage').attr('src', 'assets/images/timesUp.jpg').removeClass('invisible');
+        $('#reactionImage').attr('src', 'assets/images/timesUp.jpg').css('display', 'inline-block');
         questionIndex++; //ITERATION
         $('#questionBar').css('display', 'none');
         $('.answerBar').css('display', 'none');  
@@ -40,19 +60,26 @@ function timerTick() {
 //NEXT QUESTION REQUEST
 function startNextQuestion() {
     if (questionIndex === 8) { //GAME OVER MAN
-        console.log('triggered');
-        $('#reactionImage').addClass('invisible');
+        questionIndex = 0;
+        $('#reactionImage').css('display','none');
         $('.timeBar').addClass('invisible');
         $('#firstAnswer, #secondAnswer, #thirdAnswer').removeClass('btn btn-warning');
         $('#questionBar').css('display','block').text("All done! Here's how you did:");
         $('.answerBar').css('display','inline-block');
         $('#firstAnswer').text(`Correct answers: ${answersCorrect}`);
         $('#secondAnswer').text(`Incorrect answers: ${answersWrong}`);
-        $('#thirdAnswer').text(`Answers timed out: ${answersTimedOut}`);
+        $('#thirdAnswer').text(`Questions timed out: ${answersTimedOut}`);
+        answersCorrect = 0;
+        answersWrong = 0;
+        answersTimedOut = 0;
         $('#fourthAnswer').text('Play again!');
         $('.answerBar').off('click');
-        $('#fourthAnswer').on('click', function() {
-            //RESET GAME BUTTON GOES HERE
+        $('#fourthAnswer').on('click', function() {//RESET GAME BUTTON GOES HERE
+            $('#questionBar').text('Welcome... to Triassic Trivia!!').addClass('btn btn-warning btn-lg btn-block startButton').attr('type', 'button');
+            $('.answerBar').addClass('invisible');
+            $('.startButton').on('click', function() { //GAME RESTART
+                gameReset();
+            });
         })
     } else { //QUESTION & ANSWERS SETUP
         $('.timeBar').removeClass('invisible');
@@ -60,7 +87,7 @@ function startNextQuestion() {
         $('#timeLeft').text(timeLeft);
         timer = setInterval(timerTick, 1000);
         $('#questionBar').css('display', 'block');
-        $('#reactionImage').addClass('invisible');
+        $('#reactionImage').css('display', 'none');
         $('.answerBar').css('display', 'inline-block');
         $('#questionBar').text(questionArray[questionIndex]);
         $('#firstAnswer').text(answerArray[questionIndex][0]);
@@ -96,20 +123,23 @@ function startNextQuestion() {
     };
 };
 
-$('.answerBar').on('click', function() {
-    if ($(this).attr('class') === 'answerBar btn btn-warning correctAnswer') { //CORRECT ANSWER CLICKED
-        answersCorrect++;
-        $('#reactionImage').attr('src', 'assets/images/correctImg.jpg').removeClass('invisible');
-    } else { //WRONG ANSWER CLICKED
-        answersWrong++;
-        $('#reactionImage').attr('src', 'assets/images/incorrectImg.jpg').removeClass('invisible');
-    }
-    questionIndex++; //ITERATION
-    $('#questionBar').css('display', 'none');
-    $('.answerBar').css('display', 'none');  
-    $('.correctAnswer').removeClass('correctAnswer'); //CLEAR CORRECT ANSWER MARKER
-    clearInterval(timer);
-    setTimeout (function() {
-        startNextQuestion();
-    }, 2000)
-})
+// $('.answerBar').on('click', function() {
+//     if ($(this).attr('class') === 'answerBar btn btn-warning correctAnswer') { //CORRECT ANSWER CLICKED
+//         answersCorrect++;
+//         $('#reactionImage').attr('src', 'assets/images/correctImg.jpg').css('display','inline-block');
+//     } else { //WRONG ANSWER CLICKED
+//         answersWrong++;
+//         $('#reactionImage').attr('src', 'assets/images/incorrectImg.jpg').css('display', 'inline-block');
+//     }
+//     questionIndex++; //ITERATION
+//     $('#questionBar').css('display', 'none');
+//     $('.answerBar').css('display', 'none');  
+//     $('.correctAnswer').removeClass('correctAnswer'); //CLEAR CORRECT ANSWER MARKER
+//     clearInterval(timer);
+//     setTimeout (function() {
+//         startNextQuestion();
+//     }, 2000)
+// })
+
+//GAME INITIALIZE
+gameReset();
